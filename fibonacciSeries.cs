c87589abc7244710f0d7f0e64a1e5ed78eb19558
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-20 16:20:44 +0530

    Write a Code to FibonacciSeries

---
+++ fibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    public class fibonacciSeries
+    {
+        public static void Display()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("enter the number of element");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (i = 2; i<number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
