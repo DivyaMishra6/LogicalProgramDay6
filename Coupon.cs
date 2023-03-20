e5bb18272706ca4c8235f02c2d449bade00b4d3b
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-20 22:32:08 +0530

    Write a Coupon Problem

---
+++ Coupon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    public class Coupon
+    {
+        public static void Couponnumbers()
+        {
+            Console.WriteLine("Enter the length of the coupon number:");
+            int length = int.Parse(Console.ReadLine());
+
+            string couponNumber = GenerateCouponNumber(length);
+
+            Console.WriteLine("Coupon number: " + couponNumber);
+
+        }
+
+        static string GenerateCouponNumber(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars,length).Select(s => s[random.Next(s.Length)]).ToArray());
+
+        }
+    }
+}
