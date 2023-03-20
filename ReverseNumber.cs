0470bbc8728db9dcf8ba3856ae8c89d0ad8a7276
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-20 21:09:46 +0530

    Write a program for Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramDay6
+{
+    public class ReverseNumber
+    {
+        public static void call()
+        {
+            int remainder;
+            int reverse = 0;
+            int original;
+            Console.WriteLine(" Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 0)
+            {
+                
+                remainder = num % 10;
+                reverse = reverse * 10 + remainder;
+                num = num / 10;
+            }
+            Console.WriteLine("reverse num = " + reverse);
+        }
+    }
+}
