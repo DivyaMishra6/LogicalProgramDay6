9fdddcf0af22b6bbc3770d1706f67d075609bd90
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-20 16:07:05 +0530

    Welcome to Logical Program Day6

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using LogicalProgramDay6;
+
+namespace LogicalProgramDay6
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            //fibonacciSeries.Display();
+            //ReverseNumber.call();
+            //PerfectNumber.Call();
+            PrimeNumber.CheckprimeOrNot();
+
+        }
+    }
+}
